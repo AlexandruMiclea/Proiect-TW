da6cfd4b86b1130b440f29a2c9a8e8cd5caa94a7
Alexandru Miclea <alexandrumiclea@outlook.com>
2023-05-18 00:01:31 +0300

    ma las de facultate

--- script/contact.js
+++ script/contact.js
@@ -1,3 +1,23 @@
 window.onload = () => {
+    const buton = document.getElementById('submitx');
+    const comentariu = document.getElementsByClassName('comentariu');
+    const zonacom = document.getElementsByClassName('commentsection');
+
+    buton.addEventListener('click', () => {
+        const currentDate = new Date().toJSON().slice(0, 10);
+
+        const commentDiv = document.createElement('div');
+        commentDiv.classList.add('comment');
+        const commentDate = document.createElement('div');
+        commentDate.classList.add('data');
+        commentDate.textContent = `Data postarii: ${currentDate}`;
+        
+        const commentContent = document.createElement('p');
+        commentContent.classList.add('commentcontent');
+        commentContent.textContent = comentariu.value;
+        commentDiv.append(commentContent, commentContent)
     
+        zonacom.append(commentDiv);
+        console.log(commentDate);
+    });
 }
