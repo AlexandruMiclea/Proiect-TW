d54e658a1170e38635371a68e1bff8f331424b30
Alexandru Miclea <alexandrumiclea@outlook.com>
2023-05-18 13:53:08 +0300

    dropdown arata a dropdown doamness

--- script/contact.js
+++ script/contact.js
@@ -1,23 +1,53 @@
 window.onload = () => {
     const buton = document.getElementById('submitx');
-    const comentariu = document.getElementsByClassName('comentariu');
-    const zonacom = document.getElementsByClassName('commentsection');
+    const titlu = document.getElementById('titlucom'); //
+    const nume = document.getElementById('nume'); //
+    const email = document.getElementById('email')
+    const comentariu = document.querySelector('textarea');
+    const zonacom = document.getElementById('commentsection');
 
-    buton.addEventListener('click', () => {
+    buton.addEventListener('click', (event) => {
         const currentDate = new Date().toJSON().slice(0, 10);
 
         const commentDiv = document.createElement('div');
         commentDiv.classList.add('comment');
+        const commentMail = document.createElement('div');
+        commentMail.classList.add('mail');
+        const commentName = document.createElement('div');
+        commentName.classList.add('name');
+        const commentTitle = document.createElement('div');
+        commentTitle.classList.add('title');
         const commentDate = document.createElement('div');
         commentDate.classList.add('data');
         commentDate.textContent = `Data postarii: ${currentDate}`;
-        
+        commentDate.style.textAlign = "left";
+        const commentElem = document.createElement('div');
+        commentElem.style.display = "grid";
+        commentElem.style.gridTemplateColumns = "repeat(3,1fr)";
+        commentElem.style.margin = "1vh";
+
         const commentContent = document.createElement('p');
         commentContent.classList.add('commentcontent');
         commentContent.textContent = comentariu.value;
-        commentDiv.append(commentContent, commentContent)
+        commentTitle.textContent = titlu.value;
+        commentName.textContent = nume.value;
+        commentMail.textContent = email.value;
+        commentMail.style.textAlign = "right";
+        commentElem.append(commentDate);
+        commentElem.append(commentName);
+        commentElem.append(commentMail);
+        commentElem.style.margin = "2vh";
+        commentDiv.append(commentElem);
+        commentTitle.style.margin = "2vh";
+        commentTitle.style.fontSize = "3.5vh";
+        commentTitle.style.color = "black";
+        commentDiv.append(commentTitle)
+        commentContent.style.color = "rgb(115, 139, 47)";
+        commentDiv.append(commentContent);
     
         zonacom.append(commentDiv);
         console.log(commentDate);
+
+        event.preventDefault();
     });
 }
